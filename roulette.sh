9d33dfa1625f5e442037298da09164b7e9d83120
malori <luoskaaa@gmail.com>
2026-03-08 17:13:56 +0100

    je me suis emballée sur la roulette

--- roulette.sh
+++ roulette.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-PS3="Quelle couleur miser ?"
-options=("Tout sur le noir" "Tout sur le rouge")
-
+PS3="Quelle couleur miser ? 
+"
+options=("Tout sur le noir !!" "Tout sur le rouge !!")
+argent=1
+probabilite=50
     select choix in "${options[@]}"; do
         r=$((RANDOM%2))
+        printf "."
+        sleep 1
+        printf " ."
+        sleep 1
+        printf " .\n" 
+        sleep 2
+
         echo ${options[$r]#*le}
+        
         if [ $r -eq 0 ] ; then
-        echo "JACKPOT"
+        printf ""
+        probabilite=$(($probabilite/2))
+        argent=$((argent*2))
+        printf "JACKPOT \nvous avez $argent€ ( $probabilite %% de chance d'aller aussi loin ) \n" 
         else
-        echo "perdu"
-        fi
+        printf "perdu...\n"
         break
+        fi
     done
+    
